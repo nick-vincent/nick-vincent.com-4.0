97a7374c00c45b3d41f464ad001a81191fb406ce
Nick Vincent <the.nick.vincent@gmail.com>
2022-08-31 18:36:05 -0700

    Refactor image page data

--- src/routes/etcetera/faces/[slug]/+page.svelte
+++ src/routes/etcetera/faces/[slug]/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { page } from '$app/stores';
+	import dayjs from 'dayjs';
 	import slugify from 'slugify';
 	import Scroller from '$lib/Scroller.svelte';
 	import Lightbox from '$lib/Lightbox.svelte';
@@ -9,45 +10,45 @@
 		as: 'w=1920&h=1200&webp&quality=100&fit=inside&meta=src;aspect;width;height'
 	});
 
-	const images = [...data];
-	for (const image of images) {
+	const images = [];
+	for (let i = 0; i < data.length; i++) {
+		const image = { ...data[i] };
 		image.slug = `${image.date}-${slugify(image.title, { lower: true, strict: true })}`;
-		image.src = files[`../../../../img/faces/${image.slug}.png`].src;
+		image.path = `../../../../img/faces/${image.slug}.png`;
 		image.url = `/etcetera/faces/${image.slug}/`;
+		image.title = `${image.title}.`;
+		image.date = dayjs(image.date).format('MMMM D, YYYY');
+		image.caption = `“${image.caption}”`;
+		images[i] = image;
 	}
 
-	const face = images.find((f) => f.slug === $page.params.slug);
-	const index = images.indexOf(face);
-	const prevFace = images[index - 1] || images[images.length - 1];
-	const nextFace = images[index + 1] || images[0];
+	const image = images.find((i) => i.slug === $page.params.slug);
+	const index = images.indexOf(image);
+	const prevImage = images[index - 1] || images[images.length - 1];
+	const nextImage = images[index + 1] || images[0];
 
-	const { id, title, slug, date, caption } = face;
-
-	const imagePath = `../../../../img/faces/${slug}.png`;
-	const image = import.meta.globEager('../../../../img/faces/*.png', {
-		as: 'w=1920&h=1200&webp&quality=100&fit=inside&meta=src;aspect;width;height'
-	})[imagePath];
-
-	const { src, aspect, width, height } = image;
+	const metadata = files[image.path];
+	const { id, title, date, caption } = image;
+	const { src, aspect, width, height } = metadata;
 </script>
 
 <svelte:head>
-	<title>Nick Vincent → Etcetera → Faces → {title}.</title>
-	<meta name="description" content={`“${caption}”`} />
+	<title>Nick Vincent → Etcetera → Faces → {title}</title>
+	<meta name="description" content={caption} />
 </svelte:head>
 
 <Scroller width="40rem">
 	<Lightbox
 		{src}
 		{date}
+		{title}
+		{caption}
 		{width}
 		{height}
 		{aspect}
-		title={`${title}.`}
-		caption={`“${caption}”`}
 		backUrl="/etcetera/faces/"
-		nextUrl={`/etcetera/faces/${nextFace.slug}`}
-		previousUrl={`/etcetera/faces/${prevFace.slug}`}
+		nextUrl={nextImage.url}
+		previousUrl={prevImage.url}
 		instagramUrl={`https://instagram.com/p/${id}`}
 	/>
 </Scroller>
--- src/routes/etcetera/photos/[slug]/+page.svelte
+++ src/routes/etcetera/photos/[slug]/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { page } from '$app/stores';
+	import dayjs from 'dayjs';
 	import slugify from 'slugify';
 	import Scroller from '$lib/Scroller.svelte';
 	import Lightbox from '$lib/Lightbox.svelte';
@@ -9,26 +10,24 @@
 		as: 'w=1920&h=1200&webp&quality=100&fit=inside&meta=src;aspect;width;height'
 	});
 
-	const images = [...data];
-	for (const image of images) {
+	const images = [];
+	for (let i = 0; i < data.length; i++) {
+		const image = { ...data[i] };
 		image.slug = `${image.date}-${slugify(image.title, { lower: true, strict: true })}`;
-		image.src = files[`../../../../img/photos/${image.slug}.jpg`].src;
+		image.path = `../../../../img/photos/${image.slug}.jpg`;
 		image.url = `/etcetera/photos/${image.slug}/`;
+		image.date = dayjs(image.date).format('MMMM D, YYYY');
+		images[i] = image;
 	}
 
-	const photo = images.find((f) => f.slug === $page.params.slug);
-	const index = images.indexOf(photo);
-	const prevFace = images[index - 1] || images[images.length - 1];
-	const nextFace = images[index + 1] || images[0];
+	const image = images.find((i) => i.slug === $page.params.slug);
+	const index = images.indexOf(image);
+	const prevImage = images[index - 1] || images[images.length - 1];
+	const nextImage = images[index + 1] || images[0];
 
-	const { id, title, slug, date, caption } = photo;
-
-	const imagePath = `../../../../img/photos/${slug}.jpg`;
-	const image = import.meta.globEager('../../../../img/photos/*.jpg', {
-		as: 'w=1920&h=1200&webp&quality=100&fit=inside&meta=src;aspect;width;height'
-	})[imagePath];
-
-	const { src, aspect, width, height } = image;
+	const metadata = files[image.path];
+	const { id, title, date, caption } = image;
+	const { src, aspect, width, height } = metadata;
 </script>
 
 <svelte:head>
@@ -41,13 +40,13 @@
 		{src}
 		{date}
 		{title}
+		{caption}
 		{width}
 		{height}
 		{aspect}
-		{caption}
 		backUrl="/etcetera/photos/"
-		nextUrl={`/etcetera/photos/${nextFace.slug}`}
-		previousUrl={`/etcetera/photos/${prevFace.slug}`}
+		nextUrl={nextImage.url}
+		previousUrl={prevImage.url}
 		instagramUrl={`https://instagram.com/p/${id}`}
 	/>
 </Scroller>
