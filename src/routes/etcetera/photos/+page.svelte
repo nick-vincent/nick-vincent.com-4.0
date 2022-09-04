19dd9ba367d8292418db7a5d613d6a0687067479
Nick Vincent <the.nick.vincent@gmail.com>
2022-09-04 16:22:10 -0700

    Update photo description

--- src/routes/etcetera/photos/+page.svelte
+++ src/routes/etcetera/photos/+page.svelte
@@ -20,12 +20,12 @@
 
 <svelte:head>
 	<title>Nick Vincent → Etcetera → Photos</title>
-	<meta name="description" content="Pictures at various locales around the world." />
+	<meta name="description" content="Photography from various locales around the world." />
 </svelte:head>
 
 <Scroller width="40rem">
 	<h1>Photos</h1>
-	<p>Pictures at various locales around the world.</p>
+	<p>Photography from various locales around the world.</p>
 	<Gallery {images} />
 </Scroller>
 
