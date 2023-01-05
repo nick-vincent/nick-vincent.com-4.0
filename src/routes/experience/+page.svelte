f879a2737228da4d16910bf683eae5a1543dc8e0
Nick Vincent <the.nick.vincent@gmail.com>
2023-01-05 14:16:13 -0800

    Additional experience page refinements

--- src/routes/experience/+page.svelte
+++ src/routes/experience/+page.svelte
@@ -15,7 +15,7 @@
 		>
 	</h2>
 	<p>Senior Frontend Engineer</p>
-	<p class="date">Apr 2014 – Present</p>
+	<p class="date">Dec 2021 – Present</p>
 	<p>
 		Currently using <a href="https://stenciljs.com" rel="external noreferrer" target="_blank"
 			>Stencil</a
@@ -23,25 +23,22 @@
 		to construct cross‑framework Web&nbsp;Components for Market, Square’s shared design system used across
 		an ever‑increasing multitude of web applications
 	</p>
-	<p>
-		Previously for <a href="https://squareup.com" rel="external noreferrer" target="_blank"
-			>Square</a
-		>:
-	</p>
+
+	<hr />
+
+	<h2>
+		<a class="arrow-link" href="https://squareup.com" rel="external noreferrer" target="_blank"
+			>Square, Inc.</a
+		>
+	</h2>
+	<p>Senior Frontend Engineer</p>
+	<p class="date">Apr 2014 – Dec 2021</p>
 	<ul class="bullet spread">
 		<li>
-			Built out header navigation for global marketing website and various subsites as an internal
+			Built out header navigation for global marketing website and several subsites as an internal
 			npm library using <a href="https://svelte.dev" rel="external noreferrer" target="_blank"
 				>Svelte</a
 			>
-		</li>
-		<li>
-			Authored and maintained the internal <a
-				href="https://rubyonrails.org"
-				rel="external noreferrer"
-				target="_blank">Rails</a
-			> library housing the design system powering the global marketing website, support center, hardware
-			e‑commerce store, and other public‑facing web properties
 		</li>
 		<li>
 			Rebuilt the global marketing website to integrate with <a
@@ -49,6 +46,14 @@
 				rel="external noreferrer"
 				target="_blank">Contentful</a
 			> to move content management out of code and into the hands of product teams and content authors
+		</li>
+		<li>
+			Authored the <a href="https://rubyonrails.org" rel="external noreferrer" target="_blank"
+				>Rails</a
+			>
+			& <a href="https://jquery.com/" rel="external noreferrer" target="_blank">jQuery</a>library
+			housing the design system powering the global marketing website, support center, hardware
+			store, and other public‑facing websites
 		</li>
 	</ul>
 	<hr />
@@ -60,7 +65,7 @@
 	</h2>
 	<p>Senior Developer</p>
 	<p class="date">May 2011 – Apr 2014</p>
-	<p>Lead UX design and UI development on various e‑commerce experiments</p>
+	<p>Lead UX design and UI development on assorted e‑commerce experiments</p>
 	<hr />
 
 	<h2>
@@ -78,11 +83,8 @@
 	<hr />
 
 	<h2>
-		<a
-			class="arrow-link"
-			href="https://www.rrpartners.com"
-			rel="external noreferrer"
-			target="_blank">R&R Partners</a
+		<a class="arrow-link" href="https://rrpartners.com" rel="external noreferrer" target="_blank"
+			>R&R Partners</a
 		>
 	</h2>
 	<p>Web Developer</p>
@@ -99,7 +101,7 @@
 			>Eatdrink</a
 		>
 	</h2>
-	<p>Web Intern to Interactive Lead</p>
+	<p>Intern to Interactive Lead</p>
 	<p class="date">Mar 2003 - Apr 2009</p>
 	<p>
 		“The Web Guy” at a Vegas‑based boutique creative agency known for animation, motion graphics,
